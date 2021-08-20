541e8976d71709a5fadd69afed3aab73253d8940
Ray Messina <rmessina10@gmail.com>
2021-08-20 16:56:27 -0500

    Views_mod; adds dialogue to play_mods

--- play_mod.py
+++ play_mod.py
@@ -9,8 +9,29 @@ def play():
     result: 0
     if player_pt > 21 or house_pt > player_pt:
         result = -1
+        print("You Lose!!")
     elif player_pt > house_pt:
         result = 1.5 if player_hand == 2 else 1
-
+        if (result == 1.5):
+            print("BLACKJACK!!!")
+        print("You Win!!")
+    else:
+        print("PUSH")
     # return random.choice([-1, -1, -1, 0, 1, 2])
     return result
+
+
+def tally(hand):
+    sum = 0
+    aces = 0
+    for card in hand:
+        if card.face == "A":
+            aces += 1
+        else:
+            sum += card.value
+    sum = sum + (aces * 11)
+    for x in range(0, aces):
+        if sum > 21:
+            sum -= 10
+
+    return sum

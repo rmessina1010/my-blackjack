ccf2ffceeada698f44a2af3ac19439ef49af7acc
Ray Messina <rmessina10@gmail.com>
2021-09-01 18:15:05 -0500

    views_mod handle type and key exeptions

--- views_mod.py
+++ views_mod.py
@@ -1,20 +1,22 @@
 def show_hands(house, player, hide):
     house_str = ""
     player_str = ""
-    for card in house:
-        if card is False:
-            return
-        if hide and house_str == "":
-            house_str = "?? | "
-            continue
-        house_str += card['face'] + card['suit'] + " | "
-    for card in player:
-        player_str += card['face'] + card['suit'] + " | "
-    print("\nThe house has: " + house_str)
-    print("You have: " + player_str + "\n")
+    try:
+        for card in house:
+            if hide and house_str == "":
+                house_str = "?? | "
+                continue
+            house_str += card['face'] + card['suit'] + " | "
+        for card in player:
+            player_str += card['face'] + card['suit'] + " | "
+        print("\nThe house has: " + house_str)
+        print("You have: " + player_str + "\n")
+    except (KeyError, TypeError):
+        return
 
 
 def show_draw(who, card):
-    if card is False:
+    try:
+        print(who, "drew a " + card['face'] + card['suit'])
+    except (TypeError, KeyError):
         return
-    print(who + " drew a " + card['face'] + card['suit'])

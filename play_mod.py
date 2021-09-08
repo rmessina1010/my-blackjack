a06f459d48b5d92614e5cafa82d849d246014ec7
Ray Messina <rmessina10@gmail.com>
2021-09-07 22:14:29 -0500

    Corrects tally function; calls tally on each draw

--- play_mod.py
+++ play_mod.py
@@ -3,8 +3,6 @@ from views_mod import show_draw, show_hands
 
 
 def play(ref_deck):
-    house_pt = 0
-    player_pt = 0
     player_hand = []
     house_hand = []
     reshuffle_at = ref_deck.depth * 52 * .25
@@ -12,7 +10,8 @@ def play(ref_deck):
     result = 0
 
     deal_in(ref_deck, house_hand, player_hand)
-    user_plays(player_hand, player_pt, ref_deck, house_hand)
+    house_pt = 0
+    player_pt = user_plays(player_hand, ref_deck, house_hand)
 
     if player_pt > 21 or (house_pt > player_pt and house_pt < 22):
         result = -1
@@ -41,32 +40,34 @@ def house_plays(house_pt, player_pt, house_hand, ref_deck):
     return house_hand
 
 
-def user_plays(player_hand, player_pt, ref_deck, house_hand):
+def user_plays(player_hand,  ref_deck, house_hand):
+    print(player_hand)
+    player_pt = tally(player_hand)
     while True:
         choice = False
         while choice != "h" and choice != "s":
             choice = input("[H]it or [S]tand? ").lower()[0]
         if choice == "s":
-            break
+            return player_pt
         new_card = ref_deck.draw_card()
         show_draw("You ", new_card)
         player_hand.append(new_card)
-        player_pt += new_card["value"]
+        player_pt = tally(player_hand)
        # print(player_hand)
         show_hands(house_hand, player_hand, True)
         if player_pt > 21:
             print("You Bust!!")
-            break
+            return player_pt
 
 
 def tally(hand):
     sum = 0
     aces = 0
     for card in hand:
-        if card.face == "A":
+        if card['face'] == "A":
             aces += 1
         else:
-            sum += card.value
+            sum += card['value']
     sum = sum + (aces * 11)
     for i in range(0, aces):
         if sum > 21:
